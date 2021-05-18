b33ca9e9e7923cf6cbafd483cf0c73888ba8b4e5
Philipeano <philipeano@gmail.com>
2021-05-18 16:51:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import * as sum from './modules/sumOfNumbers.mjs';
+
+
+sum.sumOrProduct();
+
+sum.sumOrProduct('*', 5);
+
+sum.sumOrProduct(5, '*');
+
+sum.sumOrProduct(5, '+');
+
+sum.sumOrProduct(15, '/');
